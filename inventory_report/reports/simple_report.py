2ecf0ceb240d75bfd18307bceca92db83e508705
Pedro Risso <pmerchiorattor@gmail.com>
2021-07-19 20:22:33 -0300

    fix: returned values

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,22 +1,26 @@
-# -*- coding: utf-8 -*-
+# -*- coding: iso-8859-1 -*-
 from collections import Counter
+from datetime import datetime
 
 
 class SimpleReport:
     @classmethod
     def generate(self, productsList):
         min_manufactured_date = min(
-            pdt['data_de_fabricacao']for pdt in productsList
+            pdt["data_de_fabricacao"] for pdt in productsList
         )
         closest_expiration_date = min(
-            pdt['data_de_validade'] for pdt in productsList
+            pdt["data_de_validade"]
+            for pdt in productsList
+            if pdt["data_de_validade"] > datetime.today().strftime("%Y-%m-%d")
         )
-
         companies = Counter(
-            p['nome_da_empresa'] for p in productsList
+            p["nome_da_empresa"] for p in productsList
         ).most_common()
-        res = f'Data de fabricação mais antiga: {min_manufactured_date}' \
-            f'Data de validade mais próxima: {closest_expiration_date}' \
-            'Empresa com maior quantidade de produtos estocados: ' \
-            f'{companies[0][0]}'
-        return res
+
+        return (
+            f"Data de fabricação mais antiga: {min_manufactured_date}\n"
+            f"Data de validade mais próxima: {closest_expiration_date}\n"
+            "Empresa com maior quantidade de produtos estocados: "
+            f"{companies[0][0]}\n"
+        )
