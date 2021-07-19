c0ca4d281af2c6dc97492e2969c88ebabd1f9406
Bruno <bruno_grb@hotmail.com>
2021-07-19 14:26:40 -0300

    iniciando o projeto

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,18 @@
+class SimpleReport:
+    def __init__(
+        self,
+        id,
+        nome_do_produto,
+        nome_da_empresa,
+        data_de_fabricacao,
+        data_de_validade,
+        numero_de_serie,
+        instrucoes_de_armazenamento,
+    ):
+        self.id = id
+        self.nome_do_produto = nome_do_produto
+        self.nome_da_empresa = nome_da_empresa
+        self.data_de_fabricacao = data_de_fabricacao
+        self.data_de_validade = data_de_validade
+        self.numero_de_serie = numero_de_serie
+        self.instrucoes_de_armazenamento = instrucoes_de_armazenamento
