fdcfa9b458c5a523ed82a7c31c76b4614be28d51
Bruno <bruno_grb@hotmail.com>
2021-07-19 15:31:07 -0300

    requisito 1

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,18 +1,38 @@
+import datetime
+
+
 class SimpleReport:
-    def __init__(
-        self,
-        id,
-        nome_do_produto,
-        nome_da_empresa,
-        data_de_fabricacao,
-        data_de_validade,
-        numero_de_serie,
-        instrucoes_de_armazenamento,
-    ):
-        self.id = id
-        self.nome_do_produto = nome_do_produto
-        self.nome_da_empresa = nome_da_empresa
-        self.data_de_fabricacao = data_de_fabricacao
-        self.data_de_validade = data_de_validade
-        self.numero_de_serie = numero_de_serie
-        self.instrucoes_de_armazenamento = instrucoes_de_armazenamento
+    @staticmethod
+    def manufacture_date(data):
+        max_manufacture_date = datetime.timedelta.max
+        for oldest_manufacture in data:
+            if oldest_manufacture["data_de_fabricacao"] < max_manufacture_date:
+                max_manufacture_date = oldest_manufacture["data_de_fabricacao"]
+        return max_manufacture_date
+
+    @staticmethod
+    def validity(data):
+        today = datetime.today().strftime("%Y-%m-%d")
+        closest_validity = datetime.timedelta.max
+        for validity in data:
+            if validity["data_de_validade"] > today:
+                if validity["data_de_validade"] < closest_validity:
+                    closest_validity = validity["data_de_validade"]
+        return closest_validity
+
+    @staticmethod
+    def company_with_more_itens(data):
+        company = max([name["nome_da_empresa"] for name in data])
+        return company
+
+    @staticmethod
+    def generate(data):
+        max_manufacture_date = SimpleReport.manufacture_date(data)
+        closest_validity = SimpleReport.validity(data)
+        company_name = SimpleReport.company_with_more_itens(data)
+        return (
+            "Data de fabricação mais antiga: %s\n"
+            "Data de validade mais próxima: %s\n"
+            "Empresa com maior quantidade de produtos estocados: %s\n"
+            % (max_manufacture_date, closest_validity, company_name)
+        )
