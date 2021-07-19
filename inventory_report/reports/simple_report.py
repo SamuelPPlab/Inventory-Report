b12846c030768775c94c083b34c1ba5bd24899cb
Vanessa Oliveira <vanessanaara@gmail.com>
2021-07-19 18:57:20 -0300

    Correçao req 1

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -12,21 +12,21 @@ class SimpleReport:
         daysToExp = 0
         result = ''
         namesCompany = []
+
         for item in data:
             dateFab = date.fromisoformat(item['data_de_fabricacao'])
             if (olderFab > dateFab):
                 olderFab = dateFab
 
-        for item in data:
             exDat = (date.fromisoformat(item['data_de_validade']) - today)
             if (daysToExp == 0 or daysToExp > exDat.days) and exDat.days > 0:
                 daysToExp = exDat.days
                 print(daysToExp)
                 result = date.fromisoformat(item['data_de_validade'])
 
-        for item in data:
             companyName = item['nome_da_empresa']
             namesCompany.append(companyName)
+
         companyName, _ = Counter(namesCompany).most_common()[0]
 
         return f"""Data de fabricação mais antiga: {olderFab}
