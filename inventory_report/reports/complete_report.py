ca2a1116d4577843262f61e7b4fbfdff10ff8fc0
kramer <kramerscs@gmail.com>
2021-07-20 11:33:02 -0300

    fix: most common

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,5 +1,4 @@
 from inventory_report.reports.simple_report import SimpleReport
-from collections import Counter
 
 
 class CompleteReport(SimpleReport):
@@ -11,13 +10,22 @@ class CompleteReport(SimpleReport):
         most_common = inventory_per_company(inv_products)
         first_line = "Produtos estocados por empresa: "
         bottom_report = (f"""{first_line}
-- {most_common[1][0]}: {most_common[1][1]}
-- {most_common[0][0]}: {most_common[0][1]}
-- {most_common[2][0]}: {most_common[2][1]}\n""")
+{normalize_most_common(most_common)}""")
         return simple_report + "\n" + bottom_report
 
 
 def inventory_per_company(stock):
+    most_common_list = []
     lst = [item["nome_da_empresa"] for item in stock]
-    report = Counter(lst)
-    return report.most_common()
+    for item in lst:
+        tp = (item, lst.count(item))
+        if most_common_list.count(tp) == 0:
+            most_common_list.append(tp)
+    return most_common_list
+
+
+def normalize_most_common(commons):
+    most_common = ""
+    for index in range(0, len(commons)):
+        most_common += f"- {commons[index][0]}: {commons[index][1]}\n"
+    return most_common
