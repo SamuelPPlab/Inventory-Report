f7f111a27c498b41884ffd4880a39d0a7225049f
Fernando Antunes <fernando.antunes1@gmail.com>
2021-07-19 23:04:46 -0300

    Requisito 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
+import xmltodict
 
 
 class Generate_Report:
@@ -10,6 +11,24 @@ class Generate_Report:
             return SimpleReport.generate(lista)
         if tipo_de_relatorio == "completo":
             return CompleteReport.generate(lista)
+
+
+class Serialize:
+    def xml_to_list(data):
+        lista = []
+        for item in data:
+            empresa = {}
+            empresa["id"] = item["id"]
+            empresa["nome_do_produto"] = item["nome_do_produto"]
+            empresa["nome_da_empresa"] = item["nome_da_empresa"]
+            empresa["data_de_fabricacao"] = item["data_de_fabricacao"]
+            empresa["data_de_validade"] = item["data_de_validade"]
+            empresa["numero_de_serie"] = item["numero_de_serie"]
+            empresa["instrucoes_de_armazenamento"] = item[
+                "instrucoes_de_armazenamento"
+            ]
+            lista.append(empresa)
+        return lista
 
 
 class Inventory:
@@ -25,4 +44,11 @@ class Inventory:
         if path.split(".")[1] == "json":
             with open(path, mode="r") as arquivo:
                 lista = json.load(arquivo)
+            return Generate_Report.generate(lista, tipo_de_relatorio)
+
+        if path.split(".")[1] == "xml":
+            with open(path, mode="r") as arquivo:
+                reader = xmltodict.parse(arquivo.read())
+            data = reader["dataset"]["record"]
+            lista = Serialize.xml_to_list(data)
             return Generate_Report.generate(lista, tipo_de_relatorio)
