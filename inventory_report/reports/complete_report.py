7a9a520d74825f3fa506dbe39217f44fcb8ae756
Kesley Lahoz <namielahoz@gmail.com>
2021-07-19 21:34:18 -0300

    lint fixed again

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -5,14 +5,14 @@ class CompleteReport(SimpleReport):
     def generate(companies_list):
         simple_report = SimpleReport.generate(companies_list)
         companies = [company["nome_da_empresa"] for company in companies_list]
-        companies_products_quantity = ""
+        companies_products_qnt = ""
         for company in companies:
-            company_quantity = companies.count(company)
-            if company not in companies_products_quantity:
-                companies_products_quantity += f"- {company}: {company_quantity}\n"
+            company_qnt = companies.count(company)
+            if company not in companies_products_qnt:
+                companies_products_qnt += f"- {company}:{company_qnt}\n"
 
         return (
             f"{simple_report}\n"
             "Produtos estocados por empresa: \n"
-            f"{companies_products_quantity}"
+            f"{companies_products_qnt}"
         )
