c12aa2084937feb72b24bee6bce7ad11f716a902
marcelo ivan <sagacello@hotmail.com>
2021-07-20 18:19:47 -0300

    de novo

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -7,9 +7,9 @@ from json import load
 
 class Inventory:
     def load_by_extension(path, content):
-        if path.endswith(".csv"):
+        if path.endswith('.csv'):
             return list(DictReader(content))
-        elif path.endswith(".json"):
+        elif path.endswith('.json'):
             return load(content)
         else:
             path.endswith(".xml")
@@ -21,12 +21,11 @@ class Inventory:
             )
 
     def import_data(path, report_type):
-        with open(path, mode="r") as conteudo:
-            if report_type == "simples":
-                return SimpleReport.generate(
-                    Inventory.load_by_extension(path, conteudo)
-                )
+        with open(path, mode='r') as content:
+            content_dict = Inventory.load_by_extension(path, content)
+            if report_type == 'simples':
+                return SimpleReport.generate(content_dict)
             else:
-                return CompleteReport.generate(
-                    Inventory.load_by_extension(path, conteudo)
-                )
+                report_type == 'completo'
+                return CompleteReport.generate(content_dict)
+            return None
