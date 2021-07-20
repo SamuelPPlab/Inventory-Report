7c4d24d8851f2ddf28c36c9e78b45969c569e163
Fernando Antunes <fernando.antunes1@gmail.com>
2021-07-20 10:30:04 -0300

    Requisito 6 - Implementado classe JsonImporter

--- inventory_report/importer/json_importer.py
+++ inventory_report/importer/json_importer.py
@@ -1,5 +1,12 @@
 from .importer import Importer
+import json
 
 
 class JsonImporter(Importer):
-    pass
+    def import_data(path):
+        if path.split(".")[1] == "json":
+            with open(path, mode="r") as arquivo:
+                lista = json.load(arquivo)
+            return lista
+        else:
+            raise ValueError("Arquivo inválido")
