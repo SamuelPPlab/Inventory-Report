44b4295483a26cf9f113c045e4ac2ef167aa17a0
Vanessa Oliveira <vanessanaara@gmail.com>
2021-07-20 02:16:48 -0300

    Req 3

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,21 @@
+from csv import DictReader
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+    def __init__(self, receive_file, report_type):
+        self.file = receive_file
+        self.report_type = report_type
+
+    def import_data(receive_file, report_type):
+        data = []
+        with open(receive_file, 'r') as file:
+            csvReader = DictReader(file)
+            for rows in csvReader:
+                data.append(rows)
+
+        if report_type == 'simples':
+            return SimpleReport.generate(data)
+        elif report_type == 'completo':
+            return CompleteReport.generate(data)
