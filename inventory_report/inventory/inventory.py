57929aec9020462ca2fe068502e12bae7a4a5565
Kesley Lahoz <namielahoz@gmail.com>
2021-07-19 22:28:26 -0300

    Third Requirement

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,26 @@
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+import json
+import csv
+
+
+class Inventory:
+    def import_data(path, report_type):
+        if path.split(".")[1] == "csv":
+            with open(path, mode="r") as file:
+                file_reader = csv.DictReader(file)
+                list = []
+                for string in file_reader:
+                    list.append(string)
+            return Generate_Report.generate(list, report_type)
+
+        if path.split(".")[1] == "json":
+            with open(path, mode="r") as file:
+                list = json.load(file)
+            return Generate_Report.generate(list, report_type)
+
+class Generate_Report:
+    def generate(list, report_type):
+        if report_type == "simples":
+            return SimpleReport.generate(list)
+        return CompleteReport.generate(list)
