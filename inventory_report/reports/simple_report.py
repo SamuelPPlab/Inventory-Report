472325b31c36a96f2e0ef6db33ba1b6cf5feb921
Gabriel Luiz da Silva <gabrielsilva.adm@gmail.com>
2021-07-20 01:29:38 -0300

    req 2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -2,30 +2,31 @@ from datetime import datetime
 
 
 class SimpleReport:
-    def __init__(self, mock):
+    def __init__(self, report):
         pass
 
-    def generate(mock):
+    @classmethod
+    def generate(cls, report):
 
         now = datetime.today()
 
-        antiga = min(
-          [item["data_de_fabricacao"] for item in mock]
+        cls.antiga = min(
+          [item["data_de_fabricacao"] for item in report]
         )
-        proxima = min(
+        cls.proxima = min(
           [
             datetime.strptime(item["data_de_validade"], '%Y-%m-%d')
-            for item in mock
+            for item in report
           ], key=lambda x: abs(x - now))
-        quantidadeProdutos = max(
+        cls.quantidadeProdutos = max(
           [
             item["nome_da_empresa"]
-            for item in mock
-          ], key=[item["nome_da_empresa"] for item in mock].count
+            for item in report
+          ], key=[item["nome_da_empresa"] for item in report].count
         )
         return (
-              f"Data de fabricação mais antiga: {antiga}\n"
-              f"Data de validade mais próxima: {proxima.date()}\n"
+              f"Data de fabricação mais antiga: {cls.antiga}\n"
+              f"Data de validade mais próxima: {cls.proxima.date()}\n"
               f"Empresa com maior quantidade de produtos estocados: "
-              f"{quantidadeProdutos}\n"
+              f"{cls.quantidadeProdutos}\n"
         )
