00137ca1ecdb3013a5040c49557f3852bd7045e9
Fernando Antunes <fernando.antunes1@gmail.com>
2021-07-20 10:38:12 -0300

    Requisito 6 - Implementado classe XmlImporter

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -1,5 +1,15 @@
 from .importer import Importer
+from inventory_report.inventory.inventory import Serialize
+import xml.etree.ElementTree as ET
 
 
 class XmlImporter(Importer):
-    pass
+    def import_data(path):
+        if path.split(".")[1] == "xml":
+            with open(path, mode="r") as arquivo:
+                tree = ET.parse(arquivo)
+                data = tree.getroot()
+            lista = Serialize.xml_to_list(data)
+            return lista
+        else:
+            raise ValueError("Arquivo inválido")
