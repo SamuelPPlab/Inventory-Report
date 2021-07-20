bbd5065c8eb1ff43bf41175dfc674738ab91528f
Filipi Firmino <contatoic3web@gmail.com>
2021-07-20 17:25:37 -0300

    Challege 2

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,7 +1,23 @@
-from _typeshed import Self
-from simple_report import SimpleReport
+from inventory_report.reports.simple_report import SimpleReport
 
 
 class CompleteReport(SimpleReport):
-    def report():
-        Self.__dict__
+
+    def __init__(self, data):
+        self.data = data
+
+    def complete_generate(data):
+        list_company = []
+        dict_company = {}
+        for company in data:
+            list_company.append(company["nome_da_empresa"])
+        for company in list_company:
+            dict_company.setdefault(company, list_company.count(company)) 
+        return (
+            f"{SimpleReport.generate(data)} \n" +
+            "Produtos estocados por empresa: \n" +
+            f"- Forces of Nature: {dict_company['Forces of Nature']}\n" +
+            "- sanofi-aventis U.S. LLC:" +
+            f"{dict_company['sanofi-aventis U.S. LLC']}\n" +
+            f"- Newton Laboratories: {dict_company['Newton Laboratories']}\n"
+        )
