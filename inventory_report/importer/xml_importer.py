551a7cf3fa1de0ae7ff022f8d5e82e5228da28ed
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-07-21 19:51:40 -0300

    algo de errado não esta certo

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -1,19 +1,20 @@
 from inventory_report.importer.importer import Importer
-import pathlib
-import xmltodict
-import ast
-import json
+import xml.etree.ElementTree as ET
 
 
 class XmlImporter(Importer):
-    def import_data(file):
+    def import_data(path):
         data = []
-        with open(file, mode="r") as xml_file:
-            if pathlib.Path(xml_file.name).suffix != "xml":
-                raise ValueError("Arquivo inválido")
-            xml_reader = xmltodict.parse(xml_file.read())
-            xml_to_json = json.dumps(xml_reader)
-            xml_dict = ast.literal_eval(xml_to_json)["dataset"]["record"]
-            data = xml_dict
+        tree = ET.parse(path)
+        root = tree.getroot()
+        list_products = [
+            {
+                tag.tag: tag.text
+                for tag in child
+            }
+            for child in root
+        ]
+        print("XML", list_products[0])
+        return list_products
 
         return data
--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
+import xml.etree.ElementTree as ET
 
 
 class Inventory:
@@ -19,6 +20,19 @@ class Inventory:
             print("JSON", content[0])
             return content
 
+    def reader_xml(path):
+        tree = ET.parse(path)
+        root = tree.getroot()
+        list_products = [
+            {
+                tag.tag: tag.text
+                for tag in child
+            }
+            for child in root
+        ]
+        print("XML", list_products[0])
+        return list_products
+
     @classmethod
     def import_data(cls, path, type_report):
         list_products = list()
@@ -26,6 +40,8 @@ class Inventory:
             list_products = cls.reader_csv(path)
         elif path.endswith("json"):
             list_products = cls.reader_json(path)
+        elif path.endswith('xml'):
+            list_products = cls.reade_rxml(path)
 
         if type_report == "simples":
             report = SimpleReport.generate(list_products)
