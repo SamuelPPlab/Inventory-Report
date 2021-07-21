d5522a0c181d949ffe21720e964db5ae703b23f2
Rafael M Guimarães <rafaelmguimaraes@gmail.com>
2021-07-20 22:48:09 -0300

    Requisito 07 - Inventory Iterator e Refactor

---
+++ inventory_report/inventory/inventory_iterator.py
@@ -0,0 +1,20 @@
+from collections.abc import Iterator
+
+
+class InventoryIterator(Iterator):
+    _position: int = None
+    _reverse: bool = False
+
+    def __init__(self, collection, reverse: bool = False):
+        self._collection = collection
+        self._reverse = reverse
+        self._position = -1 if reverse else 0
+
+    def __next__(self):
+        try:
+            value = self._collection[self._position]
+            self._position += -1 if self._reverse else 1
+        except IndexError:
+            raise StopIteration()
+
+        return value
---
+++ inventory_report/inventory/inventory_refactor.py
@@ -0,0 +1,24 @@
+from collections.abc import Iterable
+from inventory_report.inventory.inventory_iterator import InventoryIterator
+from inventory_report.reports.complete_report import (
+    SimpleReport,
+    CompleteReport,
+)
+
+
+class InventoryRefactor(Iterable):
+    def __init__(self, importer):
+        self.data = []
+        self.importer = importer
+
+    def __iter__(self):
+        return InventoryIterator(self.data)
+
+    def import_data(self, path, type):
+        product_list = self.importer.import_data(path)
+        self.data.extend(product_list)
+        if type == "simples":
+            return SimpleReport.generate(self.data)
+        elif type == "completo":
+            return CompleteReport.generate(self.data)
+        return None
