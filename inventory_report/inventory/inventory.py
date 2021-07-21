06d16869cc5ab52b3cb72ae5314eb7e64cdaad92
mateus <mateuslaalmeida@gmail.com>
2021-07-21 19:01:06 -0300

    Refatorada classe Inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,47 +1,23 @@
-import csv
-import json
+
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
-import xml.etree.ElementTree as ET
 
 
 class Inventory:
-    def read_csv(report_path):
-        report_list = []
-        with open(report_path, mode="r") as file:
-            report_reader = csv.DictReader(file)
-            for row in report_reader:
-                report_list.append(row)
-        return report_list
-
-    def read_json(report_path):
-        with open(report_path, mode="r") as file:
-            report_reader = file.read()
-            return json.loads(report_reader)
-
-    def read_xml(report_path):
-        report_list = []
-        tree = ET.parse(report_path)
-        root = tree.getroot()
-        all_records = root.findall('record')
-        for record in all_records:
-            element = {}
-            for item in record:
-                element[item.tag] = item.text
-            report_list.append(element)
-        return report_list
-
     def import_data(report_path, report_type):
         document_type = report_path[-3:]
 
         if document_type == "csv":
-            report_list = Inventory.read_csv(report_path)
+            report_list = CsvImporter.import_data(report_path)
 
         elif document_type == "son":
-            report_list = Inventory.read_json(report_path)
+            report_list = JsonImporter.import_data(report_path)
 
         elif document_type == "xml":
-            report_list = Inventory.read_xml(report_path)
+            report_list = XmlImporter.import_data(report_path)
 
         else:
             raise ValueError("Arquivo inválido")
