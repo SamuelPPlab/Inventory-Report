c3a2aed5f0e1e34b561c411610971605d614ea17
MoisesSantana <moisaant@gmail.com>
2021-07-21 18:06:58 -0300

    req 6

--- inventory_report/importer/json_importer.py
+++ inventory_report/importer/json_importer.py
@@ -1,6 +1,11 @@
 from inventory_report.importer.importer import Importer
+import json
 
 
 class JsonImporter(Importer):
     def import_data(path_name):
-        print('json')
+        if ".json" in path_name:
+            with open(path_name) as json_file:
+                json_reader = json_file.read()
+                return json.loads(json_reader)
+        raise ValueError("Arquivo inválido")
--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -1,6 +1,21 @@
 from inventory_report.importer.importer import Importer
+import xml.etree.ElementTree as ET
 
 
 class XmlImporter(Importer):
     def import_data(path_name):
-        print('xml')
+        if ".xml" in path_name:
+            tree = ET.parse(path_name)
+            root = tree.getroot()
+            xml_result = []
+
+            for item in root.findall('./record'):
+                curr_dict = {}
+
+                for child in item:
+                    curr_dict[child.tag] = child.text
+
+                xml_result.append(curr_dict)
+
+            return xml_result
+        # raise ValueError("Arquivo inválido")
