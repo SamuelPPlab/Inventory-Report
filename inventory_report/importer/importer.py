cd93b5c51c7a21676668354c840bebbbc90e11ca
MoisesSantana <moisaant@gmail.com>
2021-07-21 18:21:19 -0300

    80%

--- inventory_report/importer/importer.py
+++ inventory_report/importer/importer.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+# https://programadoresbrasil.com.br/2021/04/classe-abstrata-em-python-entenda-como-funcionam/
+# https://qastack.com.br/programming/2052390/manually-raising-throwing-an-exception-in-python
 
 
 class Importer(ABC):
