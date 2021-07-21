fce352d3f179fbd8076ae46914dbb280c24aec82
mbjunqueiraweb <mbjunqueira@gmail.com>
2021-07-21 11:38:53 -0300

    rerefactor

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -7,14 +7,14 @@ import json
 class Inventory:
 
     def csv_reader(path):
-        with open(path, mode='r') as file: 
+        with open(path, mode="r") as file:
             content = csv.DictReader(file, delimeter=",", quotechar='"')
             products_list = [row for row in content]
             print("CSV", products_list[0])
             return products_list
 
     def json_reader(path):
-        with open(path, mode='r') as file:
+        with open(path, mode="r") as file:
             content = json.load(file)
             print("JSON", content[0])
             return content
@@ -25,7 +25,7 @@ class Inventory:
         if path.endswith("csv"):
             products_list = cls.csv_reader(path)
         elif path.endswith("json"):
-            products_list = cls.json_reade(path)
+            products_list = cls.json_reader(path)
 
         if type_report == "simples":
             report = SimpleReport.generate(products_list)
