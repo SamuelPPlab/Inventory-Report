e13437da625364304a7ae9c5aa17f577fc2c35ce
danwhat <daanwhat@gmail.com>
2021-07-21 17:13:45 -0300

    req 4 complete

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,6 +1,7 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
@@ -8,19 +9,31 @@ class Inventory:
         pass
 
     def import_data(path, modo):
-        data = []
-        with open(path) as file:
-            inventory_csv = csv.reader(file, delimiter=",", quotechar='"')
-            header, *data = inventory_csv
-            lista = list(map(lambda produto: {
-                header[0]: produto[0],
-                header[1]: produto[1],
-                header[2]: produto[2],
-                header[3]: produto[3],
-                header[4]: produto[4],
-                header[5]: produto[5],
-                header[6]: produto[6]},
-                data))
+
+        # logica JSON
+        if (path.endswith('.json')):
+            with open(path) as file:
+                lista = json.load(file)
+
+        # logica XML
+
+        # logica CSV
+        else:
+            data = []
+            with open(path) as file:
+                inventory_csv = csv.reader(file, delimiter=",", quotechar='"')
+                header, *data = inventory_csv
+                lista = list(map(lambda produto: {
+                    header[0]: produto[0],
+                    header[1]: produto[1],
+                    header[2]: produto[2],
+                    header[3]: produto[3],
+                    header[4]: produto[4],
+                    header[5]: produto[5],
+                    header[6]: produto[6]},
+                    data))
+        
+        # trabalhando com a lista:
         if modo == "simples":
             retorno = SimpleReport.generate(lista)
             return retorno
@@ -29,4 +42,4 @@ class Inventory:
             return retorno
 
 
-print(Inventory.import_data("./inventory_report/data/inventory.csv", "simples"))
+print(Inventory.import_data("./inventory_report/data/inventory.json", "completo"))
