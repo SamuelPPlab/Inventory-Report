bc7ce96e2031e7b70429464a4ff3a745a7c1767d
Gabriel Luiz da Silva <gabrielsilva.adm@gmail.com>
2021-07-21 01:10:37 -0300

    req 4 e 5 com complex

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,6 @@
 import csv
+import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -16,6 +18,28 @@ class Inventory:
                     return SimpleReport.generate(result)
                 return CompleteReport.generate(result)
             if ending == 'son':
-                print('json')
+                result = json.load(file)
+                if type == "simples":
+                    return SimpleReport.generate(result)
+                return CompleteReport.generate(result)
             if ending == 'xml':
-                print('xml')
+                root = ET.parse(path).getroot()
+                registers = root.findall("record")
+                result = []
+                for register in registers:
+                    file_dict = {}
+                    for tag in register:
+                        file_dict[tag.tag] = tag.text
+                    result.append(file_dict)
+                if type == "simples":
+                    return SimpleReport.generate(result)
+                return CompleteReport.generate(result)
+
+
+# pathTest = 'inventory_report/data/inventory.csv'
+# def teste(pathTest):
+#     with open(pathTest, "r") as file:
+#         result = [row for row in csv.DictReader(file)]
+#         return result
+# print(Inventory.import_data(pathTest, csv))
+# print(teste(pathTest))
