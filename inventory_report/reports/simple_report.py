74605783e5b22f38f32d8d9b40d3f0872d3f32e6
MoisesSantana <moisaant@gmail.com>
2021-07-20 23:49:48 -0300

    commit para pull

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,19 +1,28 @@
-# from datetime import timedelta
+from datetime import datetime
 import json
 
 
 class SimpleReport:
-    def __init__(self, content):
-        self.content = content
+    def generate(content):
+        data_list = content
 
-    def generate():
+        oldest_date = min(
+            data["data_de_fabricacao"] for data in data_list
+        )
+        print(oldest_date)
+
+        current_time = datetime.today().strftime('%Y-%m-%d')
+        nearest_expiration_date = min(
+            data["data_de_validade"]
+            for data in data_list
+            if data["data_de_validade"] > current_time
+        )
+        print(nearest_expiration_date)
 
 
 with open("./inventory_report/data/inventory.json") as file:
     result = file.read()
     content = json.loads(result)
 
-    print(content[0])
-# from datetime import date
-# >>> date.fromisoformat('2019-12-04')
-# datetime.date(2019, 12, 4)
+
+SimpleReport.generate(content)
