d7340b69aa58178178c02b4ede99ea2a8e837183
wberilo <wberilo@gmail.com>
2021-07-21 15:16:47 -0300

    fix complexity

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -9,37 +9,27 @@ class Inventory:
     def __init__(self, csv):
         pass
 
+    def import_xml(path):
+        root = ET.parse(path).getroot()
+        registers = root.findall("record")
+        result = []
+        for register in registers:
+            file_dict = {}
+            for tag in register:
+                file_dict[tag.tag] = tag.text
+            result.append(file_dict)
+        return result
+
     def import_data(path, type):
         with open(path, "r") as file:
             ending = path[-3:]
+            result = ''
             if ending == 'csv':
                 result = [row for row in csv.DictReader(file)]
-                if type == "simples":
-                    return SimpleReport.generate(result)
-                return CompleteReport.generate(result)
             if ending == 'son':
                 result = json.load(file)
-                if type == "simples":
-                    return SimpleReport.generate(result)
-                return CompleteReport.generate(result)
             if ending == 'xml':
-                root = ET.parse(path).getroot()
-                registers = root.findall("record")
-                result = []
-                for register in registers:
-                    file_dict = {}
-                    for tag in register:
-                        file_dict[tag.tag] = tag.text
-                    result.append(file_dict)
-                if type == "simples":
-                    return SimpleReport.generate(result)
-                return CompleteReport.generate(result)
-
-
-# pathTest = 'inventory_report/data/inventory.csv'
-# def teste(pathTest):
-#     with open(pathTest, "r") as file:
-#         result = [row for row in csv.DictReader(file)]
-#         return result
-# print(Inventory.import_data(pathTest, csv))
-# print(teste(pathTest))
+                result = Inventory.import_xml(path)
+            if type == "simples":
+                return SimpleReport.generate(result)
+            return CompleteReport.generate(result)
