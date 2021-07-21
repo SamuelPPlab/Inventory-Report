e000aa153d1d1246f8f502e47226934a052d4342
Rafael M Guimarães <rafaelmguimaraes@gmail.com>
2021-07-20 23:07:58 -0300

    Requisito 08 - Bonus - Main

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,23 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
 def main():
-    pass
+
+    if len(sys.argv) < 3:
+        return sys.stderr.write("Verifique os argumentos\n")
+
+    path = sys.argv[1]
+    type = sys.argv[2]
+
+    if path.endswith(".csv"):
+        inventory = InventoryRefactor(CsvImporter)
+    elif path.endswith(".json"):
+        inventory = InventoryRefactor(JsonImporter)
+    elif path.endswith(".xml"):
+        inventory = InventoryRefactor(XmlImporter)
+
+    print(inventory.import_data(path, type), end="")
