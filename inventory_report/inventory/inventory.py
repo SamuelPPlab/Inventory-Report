13d9219d5a1f9bcf46c00337ae847b3c0c8a317c
mateus <mateuslaalmeida@gmail.com>
2021-07-21 15:39:50 -0300

    Iniciando requisito 4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,17 +1,29 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
 
 class Inventory:
     def import_data(report_path, report_type):
-        with open(report_path, mode="r") as file:
-            report_reader = csv.DictReader(file)
-            report_list = []
-            for row in report_reader:
-                report_list.append(row)
-            print(report_list)
+        document_type = report_path[-3:]
+        report_list = []
+        if (document_type == "csv"):
+            with open(report_path, mode="r") as file:
+                report_reader = csv.DictReader(file)
+                for row in report_reader:
+                    report_list.append(row)
+                return report_list
+        elif (document_type == "son"):
+            with open(report_path, mode="r") as file:
+                report_reader = file.read()
+                report_list = json.loads(report_reader)
+                return report_list
         if report_type == "simples":
             return SimpleReport.generate(report_list)
         else:
             return CompleteReport.generate(report_list)
+
+
+teste = Inventory.import_data("inventory_report/data/inventory.json", "simples")
+print(teste)
