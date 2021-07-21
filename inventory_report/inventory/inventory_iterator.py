aea16c6dce98c5787d66524723f9f3ce84413ad3
Melissa Gomes <mel.ge171201@gmail.com>
2021-07-21 16:27:37 -0300

    req 7 sem erros

--- inventory_report/inventory/inventory_iterator.py
+++ inventory_report/inventory/inventory_iterator.py
@@ -4,11 +4,10 @@ from collections.abc import Iterator
 class InventoryIterator(Iterator):
     def __init__(self, iter):
         self.iter = iter
-        self.index = 0 
+        self.index = 0
     
     def __next__(self):
         self.index += 1
         if self.index > len(self.iter):
             raise StopIteration()
-        return self.iter[self.index -1]
-
+        return self.iter[self.index - 1]
