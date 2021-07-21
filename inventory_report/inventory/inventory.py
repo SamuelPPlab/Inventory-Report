5c5eae37dbcfe680f629a9eb7439222655043bfc
ANDREHORMAN1994 <tavernamoderna8000@gmail.com>
2021-07-21 15:10:09 -0300

    req 05 - 21/07/2021

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
+import xml.etree.ElementTree as ET
 
 
 class Inventory:
@@ -18,6 +19,19 @@ class Inventory:
             print("JSON", content[0])
             return content
 
+    def reader_xml(path):
+        tree = ET.parse(path)
+        root = tree.getroot()
+        list_products = [
+            {
+                tag.tag: tag.text
+                for tag in child
+            }
+            for child in root
+        ]
+        print("XML", list_products[0])
+        return list_products
+
     @classmethod
     def import_data(cls, path, type_report):
         list_products = list()
@@ -25,6 +39,8 @@ class Inventory:
             list_products = cls.reader_csv(path)
         elif path.endswith("json"):
             list_products = cls.reader_json(path)
+        elif path.endswith("xml"):
+            list_products = cls.reader_xml(path)
 
         if type_report == "simples":
             report = SimpleReport.generate(list_products)
@@ -36,4 +52,4 @@ class Inventory:
 
 if __name__ == "__main__":
     inventory = Inventory()
-    inventory.import_data("data/inventory.json", "simples")
+    # inventory.import_data("inventory_report/data/inventory.xml", "simples")
--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -63,32 +63,32 @@ def test_validar_importerdata_importar_um_arquivo_json_completo():
     assert expect == report
 
 
-# def test_validar_importerdata_importar_um_arquivo_xml_simples():
-#     expect = (
-#                 "Data de fabricação mais antiga: 2019-09-06\n"
-#                 "Data de validade mais próxima: 2022-09-17\n"
-#                 "Empresa com maior quantidade de produtos estocados: "
-#                 "Target Corporation\n"
-#     )
-#     report = Inventory.import_data('inventory_report/data/inventory.xml',
-#                                    'simples')
-#     assert expect == report
+def test_validar_importerdata_importar_um_arquivo_xml_simples():
+    expect = (
+                "Data de fabricação mais antiga: 2019-09-06\n"
+                "Data de validade mais próxima: 2022-09-17\n"
+                "Empresa com maior quantidade de produtos estocados: "
+                "Target Corporation\n"
+    )
+    report = Inventory.import_data('inventory_report/data/inventory.xml',
+                                   'simples')
+    assert expect == report
 
 
-# def test_validar_importerdata_importar_um_arquivo_xml_completo():
-#     expect = (
-#                 "Data de fabricação mais antiga: 2019-09-06\n"
-#                 "Data de validade mais próxima: 2022-09-17\n"
-#                 "Empresa com maior quantidade de produtos estocados: "
-#                 "Target Corporation\n\n"
+def test_validar_importerdata_importar_um_arquivo_xml_completo():
+    expect = (
+                "Data de fabricação mais antiga: 2019-09-06\n"
+                "Data de validade mais próxima: 2022-09-17\n"
+                "Empresa com maior quantidade de produtos estocados: "
+                "Target Corporation\n\n"
 
-#                 "Produtos estocados por empresa: \n"
-#                 "- Target Corporation: 4\n"
-#                 "- Galena Biopharma: 2\n"
-#                 "- Cantrell Drug Company: 2\n"
-#                 "- Moore Medical LLC: 1\n"
-#                 "- REMEDYREPACK: 1\n"
-#     )
-#     report = Inventory.import_data('inventory_report/data/inventory.xml',
-#                                    'completo')
-#     assert expect == report
+                "Produtos estocados por empresa: \n"
+                "- Target Corporation: 4\n"
+                "- Galena Biopharma: 2\n"
+                "- Cantrell Drug Company: 2\n"
+                "- Moore Medical LLC: 1\n"
+                "- REMEDYREPACK: 1\n"
+    )
+    report = Inventory.import_data('inventory_report/data/inventory.xml',
+                                   'completo')
+    assert expect == report
