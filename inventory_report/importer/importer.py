c707b38a3869fedf3be325697c213969e9b5edf1
Berchon <berchon@gmail.com>
2021-07-22 14:45:20 -0300

    Removed comments

--- inventory_report/importer/importer.py
+++ inventory_report/importer/importer.py
@@ -3,6 +3,5 @@ from abc import ABC, abstractmethod
 
 class Importer(ABC):
     @abstractmethod
-    # @classmethod
-    def import_data(cls, path):  # , report_type):
+    def import_data(cls, path):
         raise NotImplementedError
