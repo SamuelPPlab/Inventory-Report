9b923342741de224e917f2911382f925c30ce7e5
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-07-23 17:39:58 -0300

    tentativa xml

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,19 +1,54 @@
 import csv
+import json
+import xmltodict
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
-    @classmethod
-    def import_data(cls, path_document, type_of_document):
+    def csv_report(path_document, type_of_document):
         report = ''
         file_in_dict = []
 
         with open(path_document) as f:
             file_in_dict = [row for row in csv.DictReader(f)]
+            print(file_in_dict)
+        if type_of_document == "simples":
+            report = SimpleReport.generate(file_in_dict)
+        if type_of_document == "completo":
+            report = CompleteReport.generate(file_in_dict)
+        return report
+
+    def json_report(path_document, type_of_document):
+        report = ''
+        file_in_dict = []
+        with open(path_document) as f:
+            file_in_dict = json.load(f)
 
         if type_of_document == "simples":
             report = SimpleReport.generate(file_in_dict)
         if type_of_document == "completo":
             report = CompleteReport.generate(file_in_dict)
+        return report
+
+    def xml_report(path_document, type_of_document):
+        report = ''
+        file_in_dict = []
+        with open(path_document) as f:
+            file_in_dict = xmltodict.parse(f.read())
+        if type_of_document == "simples":
+            report = SimpleReport.generate(file_in_dict)
+        if type_of_document == "completo":
+            report = CompleteReport.generate(file_in_dict)
+        return report
+
+    @classmethod
+    def import_data(cls, path_document, type_of_document):
+        report = ''
+        if path_document.endswith(".csv"):
+            report = Inventory.csv_report(path_document, type_of_document)
+        if path_document.endswith('.json'):
+            report = Inventory.json_report(path_document, type_of_document)
+        if path_document.endswith('.xml'):
+            report = Inventory.xml_report(path_document, type_of_document)
         return report
