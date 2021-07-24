f4d0f75b25069bc60c2f4973b83d1a8ac3d067b6
EmanuelleBrasil <emanuelle.rbl@gmail.com>
2021-07-24 15:48:42 -0300

    requisito 1

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,57 @@
+from datetime import date
+from statistics import mode
+
+
+def get_manufactory_date(list):
+    today = date.today()
+
+    for index, product in enumerate(list):
+        actual_date = date.fromisoformat(product["data_de_fabricacao"])
+        if index < len(list) - 2:
+            next_date = date.fromisoformat(
+                list[index + 1]["data_de_fabricacao"]
+            )
+        if today - actual_date > today - next_date:
+            oldest_date = actual_date
+        else:
+            oldest_date = next_date
+    return oldest_date.isoformat()
+
+
+def get_expiration_date(list):
+    today = date.today()
+
+    for index, product in enumerate(list):
+        actual_date = date.fromisoformat(product["data_de_validade"])
+        if today.year < actual_date.year:
+            if index < len(list) - 2:
+                next_date = date.fromisoformat(
+                    list[index + 1]["data_de_validade"]
+                )
+                if abs(actual_date.toordinal() - today.toordinal()) < abs(
+                    next_date.toordinal() - today.toordinal()
+                ):
+                    nearest_date = actual_date
+                else:
+                    nearest_date = next_date
+    return nearest_date.isoformat()
+
+
+# Referência: PR Luíse Rios
+def get_company_name(list):
+    company = mode(product["nome_da_empresa"] for product in list)
+    return company
+
+
+class SimpleReport:
+    def generate(product_list):
+        manufactoring_date = get_manufactory_date(product_list)
+        expiration_date = get_expiration_date(product_list)
+        company_name = get_company_name(product_list)
+        report = (
+            f"Data de fabricação mais antiga: {manufactoring_date}\n"
+            f"Data de validade mais próxima: {expiration_date}\n"
+            "Empresa com maior quantidade de produtos estocados: "
+            f"{company_name}\n"
+        )
+        return report
