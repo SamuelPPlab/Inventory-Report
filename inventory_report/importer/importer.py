043dfc45c8c151d1899391c2e69d1111f062830a
PedroMarquesFr2 <pedromarques1011@gmail.com>
2021-07-24 14:38:56 -0300

    ue

--- inventory_report/importer/importer.py
+++ inventory_report/importer/importer.py
@@ -3,5 +3,5 @@ from abc import ABC, abstractmethod
 
 class Importer(ABC):
     @abstractmethod
-    def import_data(file_path: str) -> list:
+    def import_data(file_path: str):
         return NotImplemented
