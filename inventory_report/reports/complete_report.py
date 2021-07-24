7be0722aa4347e6ed41f8e7ac7737c9d70915582
TIAGO <tiagoesdrasvieira@gmail.com>
2021-07-24 13:48:08 -0300

    Fix flake8

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -4,8 +4,9 @@ from collections import Counter
 
 class CompleteReport(SimpleReport):
     @classmethod
-    def generate(self, reportList):        
-        empresas = dict(Counter(data["nome_da_empresa"] for data in reportList))        
+    def generate(self, reportList):
+        empresas = dict(Counter(
+            data["nome_da_empresa"] for data in reportList))
         produtosPorEmpresas = "".join(
             f"- {data[0]}: {data[1]}\n" for data in empresas.items()
         )
