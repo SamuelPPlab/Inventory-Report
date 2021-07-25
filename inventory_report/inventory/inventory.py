87ea128ea2101d7104a353581eb4b873ced05565
TIAGO <tiagoesdrasvieira@gmail.com>
2021-07-25 10:30:31 -0300

    Requisito 4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -11,9 +12,17 @@ class Inventory:
             return list(arquivoCSV)
 
     @classmethod
+    def readJSON(self, caminho):
+        with open(caminho) as arquivo:
+            arquivoJSON = json.load(arquivo)
+            return arquivoJSON
+
+    @classmethod
     def import_data(self, caminho, tipo):
         if caminho.endswith(".csv"):
             conteudo = self.readCSV(caminho)
+        if caminho.endswith(".json"):
+            conteudo = self.readJSON(caminho)
         if tipo == "simples":
             relatorio = SimpleReport.generate(conteudo)
         else:
