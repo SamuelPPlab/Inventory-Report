c85ff3a49f1b01b1087b1ee925742ade3f2c1dd8
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-07-25 18:46:30 -0300

    Requisito 2

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,23 @@
+from string import Template
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+
+    this_template = Template("\nProdutos estocados por empresa: \n$result\n")
+
+    @classmethod
+    def generate(cls, list):
+        result = "\n".join(
+            [
+                f"- {empresa}: {quantidade}"
+                for empresa, quantidade in (
+                    Counter(item["nome_da_empresa"] for item in list).items()
+                )
+            ]
+        )
+
+        return super().generate(list) + cls.this_template.substitute(
+            result=result
+        )
