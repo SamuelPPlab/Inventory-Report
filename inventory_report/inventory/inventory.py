284012a959c1a3d69dba7e5a0c23f18a3e6cc4b5
CarolSi-hub <a.carolg1@gmail.com>
2021-07-24 21:12:38 -0300

    requisito 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
+import xml_to_dict
 
 
 class Inventory:
@@ -12,6 +13,8 @@ class Inventory:
             report = cls.open_csv(file_path)
         if file_path.endswith('.json'):
             report = cls.open_json(file_path)
+        if file_path.endswith('.xml'):
+            report = cls.open_xml(file_path)
         if report_type == "simples":
             data = SimpleReport.generate(report)
         else:
@@ -30,4 +33,10 @@ class Inventory:
             file = json.load(j_file)
             return file
 
-    
+    @classmethod
+    def open_xml(cls, file_path):
+        with open(file_path) as x_file:
+            parser = xml_to_dict.XMLtoDict()
+            xml_file = x_file.read()
+            file = parser.value_from_nest("record", xml_file)
+            return file
