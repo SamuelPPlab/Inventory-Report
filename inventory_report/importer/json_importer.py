5cce26548871410f1ba8d84b6052d6625eded350
PedroMarquesFr2 <pedromarques1011@gmail.com>
2021-07-25 19:04:37 -0300

    removido prints e corrigido req6

--- inventory_report/importer/json_importer.py
+++ inventory_report/importer/json_importer.py
@@ -2,7 +2,7 @@ import json
 from inventory_report.importer.importer import Importer
 
 
-class CsvImporter(Importer):
+class JsonImporter(Importer):
     def import_data(self, file_path: str) -> dict:
         if file_path.endswith(".json"):
             with open(file_path, mode="r") as file:
