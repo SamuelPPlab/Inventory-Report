6d8be268041c10f56aa42677e1ecaeec5aeb5b7a
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-07-25 19:01:59 -0300

    Requisito 3

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,33 @@
+import csv
+import json
+import xmltodict
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class Inventory:
+    @classmethod
+    def xml_helper(cls, content):
+        content_dict = xmltodict.parse(content.read())
+        return [dict(product) for product in content_dict["dataset"]["record"]]
+
+    @classmethod
+    def load_file(cls, path):
+        with open(path, mode="r") as content:
+            if path.endswith(".json"):
+                return json.load(content)
+            elif path.endswith(".xml"):
+                return cls.xml_helper(content)
+            elif path.endswith(".csv"):
+                return list(csv.DictReader(content))
+            else:
+                return None
+
+    @classmethod
+    def import_data(cls, path, type):
+        list = cls.load_file(path)
+        if type == "completo":
+            return CompleteReport.generate(list)
+        elif type == "simples":
+            return SimpleReport.generate(list)
+        return None
