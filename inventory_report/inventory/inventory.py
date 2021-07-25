98d73cd660b4b1f445d1d7c7ab4f2653b7525d66
brendalima <brenda.c.lima@poli.ufrj.br>
2021-07-25 18:42:58 -0300

    req 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -3,6 +3,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
+import xmltodict
 
 
 class Inventory:
@@ -22,8 +23,16 @@ class Inventory:
             content = json.load(json_file)
             return Inventory.generate_report(content, report_type)
 
+    def read_xml_file(file_path, report_type):
+        with open(file_path, 'r') as xml_file:
+            content = xmltodict.parse(xml_file.read())
+            converted_content = content["dataset"]["record"]
+            return Inventory.generate_report(converted_content, report_type)
+
     def import_data(file_path, report_type):
         if file_path.endswith('csv'):
             return Inventory.read_CSV_file(file_path, report_type)
         if file_path.endswith('json'):
             return Inventory.read_JSON_file(file_path, report_type)
+        if file_path.endswith('xml'):
+            return Inventory.read_xml_file(file_path, report_type)
