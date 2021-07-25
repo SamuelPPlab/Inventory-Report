87848b1609362ef52554819e3739a4c536a2a83b
TIAGO <tiagoesdrasvieira@gmail.com>
2021-07-25 10:36:54 -0300

    Requisito 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -18,11 +19,27 @@ class Inventory:
             return arquivoJSON
 
     @classmethod
+    def readXML(self, caminho):
+        with open(caminho) as arquivo:
+            relatorio = []
+            elementos = ET.parse(arquivo)
+            root = elementos.getroot()
+            for elemento in root.iter("record"):
+                item = {}
+                for filho in elemento.iter("*"):
+                    if filho.tag != "record":
+                        item[filho.tag] = filho.text
+                relatorio.append(item)
+            return relatorio
+
+    @classmethod
     def import_data(self, caminho, tipo):
         if caminho.endswith(".csv"):
             conteudo = self.readCSV(caminho)
         if caminho.endswith(".json"):
             conteudo = self.readJSON(caminho)
+        if caminho.endswith(".xml"):
+            conteudo = self.readXML(caminho)
         if tipo == "simples":
             relatorio = SimpleReport.generate(conteudo)
         else:
