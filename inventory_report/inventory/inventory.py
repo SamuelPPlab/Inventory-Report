0e9f8f413662fdd5acf4029d065af42efb5a1437
brendalima <brenda.c.lima@poli.ufrj.br>
2021-07-24 21:47:59 -0300

    req 4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,14 +2,14 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
     def generate_report(content, report_type):
         if report_type == 'simples':
             return SimpleReport.generate(content)
-        else:
-            return CompleteReport.generate(content)
+        return CompleteReport.generate(content)
 
     def read_CSV_file(file_path, report_type):
         with open(file_path, 'r') as csv_file:
@@ -17,6 +17,13 @@ class Inventory:
             converted_content = list(content)
             return Inventory.generate_report(converted_content, report_type)
 
+    def read_JSON_file(file_path, report_type):
+        with open(file_path, 'r') as json_file:
+            content = json.load(json_file)
+            return Inventory.generate_report(content, report_type)
+
     def import_data(file_path, report_type):
         if file_path.endswith('csv'):
             return Inventory.read_CSV_file(file_path, report_type)
+        if file_path.endswith('json'):
+            return Inventory.read_JSON_file(file_path, report_type)
