f7b7ebb1346d4b4accafca5cab5d21f3cf10c695
Jessica Ladislau <jessica.ladislau@payface.com.br>
2021-07-25 20:03:23 -0300

    feat: requirement 1

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from collections import Counter
+
+
+class SimpleReport:
+    """     @classmethod
+    def __init__(self, list):
+        self.list = list """
+
+    def generate(list):
+        today = datetime.today().strftime("%Y-%m-%d")
+
+        oldest_fabrication_date = min(
+            product["data_de_fabricacao"] for product in list
+        )
+
+        closest_expiration_date = min(
+            product["data_de_validade"] for product in list
+            if product["data_de_validade"] > today
+        )
+
+        name = Counter(
+            company["nome_da_empresa"] for company in list
+        ).most_common()[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_fabrication_date}\n"
+            f"Data de validade mais próxima: {closest_expiration_date}\n"
+            f"Empresa com maior quantidade de produtos estocados: {name}\n"
+        )
+
+
+# Luciano Berchon e Pedro Riso
