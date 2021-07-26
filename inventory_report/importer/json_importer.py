50cd7ce5325d514a2947b57e80729faee92c4d3c
CIANDT\brune <brune.freire@bradesco.com.br>
2021-07-26 20:00:37 -0300

    Correçao da implementaçao 6

--- inventory_report/importer/json_importer.py
+++ inventory_report/importer/json_importer.py
@@ -2,9 +2,11 @@ from inventory_report.importer.importer import Importer
 import json
 
 
-class Json(Importer):
+class JsonImporter(Importer):
 
     def import_data(path):
+        if not path.endswith(".json"):
+            raise ValueError("Arquivo inválido")
         with open(path) as arquivo:
             arquivoJSON = json.load(arquivo)
             return arquivoJSON
