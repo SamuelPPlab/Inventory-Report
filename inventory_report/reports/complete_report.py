add97d60ffa769006139fadaac013d2896edd724
julioKauer <juliokauer@gmail.com>
2021-07-26 09:52:22 -0300

    Requisito 2

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,22 @@
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(list):
+        simpleReport = SimpleReport.generate(list)
+
+        companiesProducts = Counter(
+            product["nome_da_empresa"] for product in list
+        ).items()
+
+        companiesProductsReport = ""
+        for key, value in companiesProducts:
+            companiesProductsReport += f"- {key}: {value}\n"
+
+        return (
+          f"{simpleReport}\n"
+          + "Produtos estocados por empresa: \n"
+          + f"{companiesProductsReport}"
+        )
