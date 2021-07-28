c916cbd387ced1fa484db42e7700dedb0b5a1b84
Raphael Caputo <faelelf2@gmail.com>
2021-07-27 22:40:34 -0300

    [Pedro Marquest e Raphael Caputo] Projeto Raphael Caputo

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -3,6 +3,8 @@ from inventory_report.inventory.inventory import Inventory
 
 
 class XmlImporter(Importer):
+    """Comentario para subir pr Raphael Caputo"""
+
     @classmethod
     def import_data(cls, xml_data):
         if not xml_data.endswith(".xml"):
