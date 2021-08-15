71e831a6966b89d5f2c28e180d766e5ea9f64c0a
flaviosoliver <flavsoliver@gmail.com>
2021-08-14 21:49:18 -0300

    req 6 - dev-requeriments

--- inventory_report/importer/xml_importer.py
+++ inventory_report/importer/xml_importer.py
@@ -4,7 +4,6 @@ from .importer import Importer
 
 
 class XmlImporter(Importer):
-    @classmethod
     def import_data(self, file_name):
         if file_name.endswith(".xml"):
             with open(file_name) as file:
