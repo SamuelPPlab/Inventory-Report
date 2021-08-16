08824727490ab4f1765039a470128875bf1cf2db
Tiago Esdras <68123518+TiagoEsdras@users.noreply.github.com>
2021-08-16 17:21:37 -0300

    Update complete_report.py

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,18 @@
+
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(self, reportList):
+        empresas = dict(Counter(
+            data["nome_da_empresa"] for data in reportList))
+        produtosPorEmpresas = "".join(
+            f"- {data[0]}: {data[1]}\n" for data in empresas.items()
+        )
+        return (
+            f"{super().generate(reportList)}\n"
+            "Produtos estocados por empresa: \n"
+            f"{produtosPorEmpresas}"
+        )
