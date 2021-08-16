a1ad98651116f4b53821912ead803cf24cf22297
Lets <letsferreiratx@gmail.com>
2021-08-15 21:19:21 -0300

    req 7 e 8

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,22 @@
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+import sys
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        return print("Verifique os argumentos", file=sys.stderr)
+
+    file_path = sys.argv[1]
+    type_repoter = sys.argv[2]
+
+    if file_path.endswith(".csv"):
+        inventory = InventoryRefactor(CsvImporter)
+    if file_path.endswith(".json"):
+        inventory = InventoryRefactor(JsonImporter)
+    if file_path.endswith(".xml"):
+        inventory = InventoryRefactor(XmlImporter)
+
+    print(inventory.import_data(file_path, type_repoter), end="")
