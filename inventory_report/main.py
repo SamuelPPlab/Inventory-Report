101c23717349f0d07935c7f086bb34604ade5ca8
josielvs <josielvs@gmail.com>
2021-08-17 19:52:45 -0300

    Requisitos 7 e 8

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,25 @@
+# def main():
+#     pass
+
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+import sys
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        return print("Verifique os argumentos", file=sys.stderr)
+
+    file = sys.argv[1]
+    type_repoter = sys.argv[2]
+
+    if file.endswith(".csv"):
+        inventory_data = InventoryRefactor(CsvImporter)
+    if file.endswith(".json"):
+        inventory_data = InventoryRefactor(JsonImporter)
+    if file.endswith(".xml"):
+        inventory_data = InventoryRefactor(XmlImporter)
+
+    print(inventory_data.import_data(file, type_repoter), end="")
